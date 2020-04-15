5d5df49315b15266ec5285d764b4ac93d848b817
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-15 15:27:34 -0700

    git.course

--- py_images.py
+++ py_images.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
-
-
+#
+#   A script that iterates over image files in a directory and makes changes
+#   to them using the Pillow library.
+#
+#   Author:     Sandro Aguilar
+#   Date:       April 15, 2020
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+import os
