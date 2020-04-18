9a1b0c2211b7ccf5e92e12f655421391ed7d913a
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-18 14:05:12 -0700

    git.course

--- py_images.py
+++ py_images.py
@@ -21,6 +21,7 @@ for file in os.listdir(directory):
         file_loc = './images/' + file
         img = Image.open(file_loc)
         print(img.format, img.size, img.mode)
+        print(file)
     # img.rotate(90).show()
 
 
