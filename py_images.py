1864478045d0f83b2ac4976d13a8661b8c8fe346
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-18 13:12:29 -0700

    git.course

--- py_images.py
+++ py_images.py
@@ -16,7 +16,7 @@ directory = './images'
 # iterate over files in directory
 for file in os.listdir(directory):
     if file.endswith('.jpg'):
-    print(file)
+        print(file)
 
 
     # img = Image.open(file)
