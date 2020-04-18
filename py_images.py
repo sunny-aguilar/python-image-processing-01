7ea9b6b4ad08e8e153e2e5bb0053ed88bc2b691a
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-18 13:48:16 -0700

    git.course

--- py_images.py
+++ py_images.py
@@ -16,7 +16,7 @@ directory = './images'
 # iterate over files in directory
 for file in os.listdir(directory):
     if file.endswith('ic_add_location_black_48dp'):
-        print('FOUND')
+        print(file)
         # img = Image.open(file)
         # img.rotate(45).show()
         # img = Image.open(file)
