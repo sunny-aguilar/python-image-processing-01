00409d84c2ab681d411c77c6519252808b50fbaa
sunny-aguilar <sunny.aguilar@gmail.com>
2020-04-17 21:30:23 -0700

    git.course

--- py_images.py
+++ py_images.py
@@ -17,7 +17,7 @@ directory = './images'
 for filename in os.listdir(directory):
     if filename.endswith('.jpg'):
         # do stuff here to file
-        print("FOUND")
+        #
         # img = Image.open('img1.jpg')
         # img.rotate(45).show()
 
